5c01c6547581ef5649471dc607cd358053263921
Goldenlady0940 <sebrinaa18@gmail.com>
2022-11-18 04:49:06 +0300

    update readme

--- binary_trees.h
+++ binary_trees.h
@@ -80,6 +80,6 @@ avl_t *avl_remove(avl_t *root, int value);
 avl_t *avl_remove(avl_t *root, int value);
 int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
-
+int binary_tree_is_avl(const binary_tree_t *tree);
 const binary_tree_t *second);
 #endif
